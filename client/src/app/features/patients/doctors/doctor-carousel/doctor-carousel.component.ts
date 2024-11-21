6b05c6392e4512a8fff86b31b28c1f710765f352
Shane Louis Barita <89013136+baristabarita@users.noreply.github.com>
2024-11-21 19:00:20 +0800

    [INIT] Added Additional starter components, installed tailwind css

---
+++ client/src/app/features/patients/doctors/doctor-carousel/doctor-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-carousel.component.html',
+  styleUrl: './doctor-carousel.component.css'
+})
+export class DoctorCarouselComponent {
+
+}
