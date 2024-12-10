f9b098079462bdf89e2b5649533ba7c9b5dc5a99
LanJRamos <lanceramos75@gmail.com>
2024-12-10 17:02:33 +0800

    feat: backend for Analytics (I added few things in AppointmentRepository)

--- api/src/main/java/com/clinicconnect/api/controller/AnalyticsController.java
+++ api/src/main/java/com/clinicconnect/api/controller/AnalyticsController.java
@@ -1,5 +1,7 @@
 package com.clinicconnect.api.controller;
 
+import com.clinicconnect.api.service.AnalyticsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,17 @@ import java.util.Map;
 
 @RestController
 public class AnalyticsController {
-    @GetMapping("/api/anaytics")
-    public Map<String, Integer> getAppointmentsPerMonth() {
-        Map<String, Integer> data = new HashMap<>();
-        //Replace with actual database logic later...
-        data.put("January", 50);
-        data.put("February", 75);
-        data.put("March", 100);
 
-        return data;
+    @Autowired
+    private AnalyticsService analyticsService;
+
+    @GetMapping("/api/appointments-per-month")
+    public Map<String, Integer> getAppointmentsPerMonth() {
+        return analyticsService.getAppointmentsPerMonth();
+    }
+
+    @GetMapping("/api/pending-appointments")
+    public Map<String, Integer> getPendingAppointments() {
+        return analyticsService.getPendingAppointments();
     }
 }
